cf1d740e1a68f80649e823e938b47c5c3491c004
Juan Carlos <143283368+Chispa8@users.noreply.github.com>
2024-09-19 13:38:39 +0100

    Update firebase.js

--- adopta-pet/src/firebase.js
+++ adopta-pet/src/firebase.js
@@ -5,13 +5,13 @@ import { getStorage } from "firebase/storage"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCEb5nnV9Uz3hIQGe87UxLPMRKRIvMhBvE",
-  authDomain: "shelter-app-e67e8.firebaseapp.com",
-  projectId: "shelter-app-e67e8",
-  storageBucket: "shelter-app-e67e8.appspot.com",
-  messagingSenderId: "319843378560",
-  appId: "1:319843378560:web:49145666425409f1ad4d65",
-  measurementId: "G-DENFP33LBK", // Este no es necesario si no usas Analytics
+  apiKey: "**********************",
+  authDomain: "*************",
+  projectId: "*********",
+  storageBucket: "***************",
+  messagingSenderId: "**********",
+  appId: "***************************",
+  measurementId: "********", 
 }
 
 // Initialize Firebase
